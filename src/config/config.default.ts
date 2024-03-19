a4b22435a2e0406c1c8948bb6bf4feb7853103ff
sunzx <sunzx@greatld.com>
2024-03-19 11:44:23 +0800

    feat: initial midway framework

---
+++ src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: 'StudentAdmissionPlatform',
+  koa: {
+    port: 7101,
+  },
+} as MidwayConfig;
